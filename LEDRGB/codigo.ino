986b50060a077a9dc9b4d5e315b1248a360221da
Carlos Estevão <39769751+carlosestevaobs@users.noreply.github.com>
2022-02-23 09:11:31 -0300

    LEDRGB

---
+++ LEDRGB/codigo.ino
@@ -0,0 +1,45 @@
+const int ledAzul = 10;      //refere-se ao pino digital 8.
+const int ledVerde = 9;     //refere-se ao pino digital 9.
+const int ledVermelho = 12; //refere-se ao pino digital 10.
+ 
+//Método setup, executado uma vez ao ligar o Arduino.
+void setup() {
+  //Definindo os pinos digitais (8, 9, 10) como de saída.
+  pinMode(ledAzul,OUTPUT);   
+  pinMode(ledVerde,OUTPUT);   
+  pinMode(ledVermelho,OUTPUT);    
+}
+ 
+//Método loop, executado enquanto o Arduino estiver ligado.
+void loop() {   
+  //Acendendo cada cor individualmente.  
+  digitalWrite(ledVermelho,HIGH);
+  delay(500);
+  digitalWrite(ledVermelho,LOW);
+   
+  digitalWrite(ledVerde,HIGH);
+  delay(500);
+  digitalWrite(ledVerde,LOW);
+   
+  digitalWrite(ledAzul,HIGH);
+  delay(500);
+  digitalWrite(ledAzul,LOW);    
+   
+   //Misturando as cores do led para obter cores diferentes.
+  digitalWrite(ledAzul,HIGH);     
+  digitalWrite(ledVerde,HIGH);
+  digitalWrite(ledVermelho,HIGH);
+  delay(1500);    
+  digitalWrite(ledAzul,HIGH);
+  digitalWrite(ledVerde,HIGH);
+  digitalWrite(ledVermelho,LOW);  
+  delay(1500);      
+  digitalWrite(ledAzul,LOW);
+  digitalWrite(ledVerde,HIGH);
+  digitalWrite(ledVermelho,HIGH);
+  delay(1500);      
+  digitalWrite(ledAzul,HIGH);
+  digitalWrite(ledVerde,LOW);
+  digitalWrite(ledVermelho,HIGH);
+  delay(1500);
+}
